ddc3f584bca843b93b4819ef5c577af28edccef0
Israel Rifarachi hoyos <irifarac@car11s4.42barcelona.com>
2024-06-10 10:31:36 +0200

    changing write

--- ft_write.s
+++ ft_write.s
@@ -6,20 +6,17 @@ global ft_write
 
 ft_write:
 	mov rax, 1	; syscall number
-	mov rdi, rdi	; file descriptor
-	mov rsi, rsi	; buffer
-	mov rdx, rdx	; buffer size
 	syscall		; call kernel
 
 	; Check for error
-	cmp rax, 0	; check for error
+	cmp rax, 0		; check for error
 	jge no_error	; no error
-	neg rax		; if rax < 0, make it positive (errno is positive)
-	mov rcx, rax	; save errno
+	neg rax				; if rax < 0, make it positive (errno is positive)
+	mov rdi, rax	; save errno
 	call __errno_location	; get errno location
-	mov [rax], rcx	; store errno
-	mov rax, -1	; return -1
-	ret		; return
+	mov [rax], rdi	; store errno
+	mov rax, -1			; return -1
+	ret
 
 no_error:
-	ret		; return
+	ret
